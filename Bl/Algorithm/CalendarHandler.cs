67c39006a8489e3f34a318911f83b468757755fb
estiZukerman <hand2b20@mbjcomp.org.il>
2023-04-24 21:40:05 +0300

    middle the FindWashAbleToWash func

--- Bl/Algorithm/CalendarHandler.cs
+++ Bl/Algorithm/CalendarHandler.cs
@@ -6,14 +6,33 @@ using System.Threading.Tasks;
 
 namespace Bl.Algorithm
 {
-    public  class CalendarHandler
+    public class CalendarHandler
     {
-        
-        public  Dictionary<DateTime, Dictionary<string, Category>> DatesLessThan24Hours(Dictionary<DateTime, Dictionary<string, Category>> datesDict) 
 
-         => datesDict.Where(k => (k.Key - DateTime.Now).TotalHours < 24 && (k.Key - DateTime.Now).TotalHours >= 0).ToDictionary(k => k.Key , v=> v.Value);
+        public Dictionary<DateTime, Dictionary<string, Category>> DatesLessThan24Hours(Dictionary<DateTime, Dictionary<string, Category>> datesDict)
 
+             => datesDict.Where(k => (k.Key - DateTime.Now).TotalHours < 24 && (k.Key - DateTime.Now).TotalHours >= 0).ToDictionary(k => k.Key, v => v.Value);
+        public Dictionary<DateTime, List<WashAbleDTO>> FindWashAbleToWash(Dictionary<DateTime, Dictionary<string, Category>> specificDateDict)
+        {
+          
+        }
 
+        public static List<WashAbleDTO> GetCleanWashables(List<WashAbleDTO> allWashAbles) => allWashAbles.FindAll(w => w.Status == Status.clean);
 
+        public bool ExistsInClean(string userID, Category category, List<WashAbleDTO> cleanWashAbles)
+            => cleanWashAbles.First(w => w.UserId == userID && w.Category == category) != null;
+
+        public WashAbleDTO NecessaryWashAble(string userID, Category category, List<WashAbleDTO> dirtyWashAbles)
+        {
+            WashAbleDTO washAble = dirtyWashAbles.First(w => w.UserId == userID && w.Category == category);
+            if (washAble != null)
+            {
+                washAble.NecessityLevel = NecessityLevel.necessary;
+                return washAble;
+            }
+            else throw new Exception("There is not exists a washable with this condition");
+
+        }
+       
     }
 }
